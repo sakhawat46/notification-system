b13841d2aa26f90cd5ec5ab914fa95084c5a9544
Sakhawat Hossain <hsakhawat135@gmail.com>
2026-05-24 11:28:42 +0600

    first commit

---
+++ projects/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # add by sakhawat
+    path('api/v1/', include('apps.users.urls')),
+    path('api/v1/', include('apps.notification.urls')),
+]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
